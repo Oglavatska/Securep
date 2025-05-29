f04f0ba73cd87162640b728003c44bf3847a6b65
Olena Glavatska <213353399+Oglavatska@users.noreply.github.com>
2025-05-29 20:25:53 +0200

    Update index.php
    Die Tabelle im DB wird vor INSERT geleert, so das es keine Doppelung entsteht. Es wurden zwei Formulare im Frontend ergänzt für Benutzerfreundlichkeit.
    Der Code wurde in Funktionen gepasst, die nur per Klick ausgeführt wurden.

--- index.php
+++ index.php
@@ -3,102 +3,175 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Log-Analysator</title>
+    <style>
+        body {
+            font-size: 18px;
+        }
+
+        .status {
+            position: absolute;
+            top: 111px;
+            left: 228px;
+            font-size: 18px;
+        }
+
+        .button-wrapper.import {
+            position: relative;
+        }
+
+        .buttons {
+            padding: 10px 15px;
+            font-size: 18px;
+            margin: 20px;
+        }
+
+        .results {
+            margin: 20px;
+        }
+    </style>
 </head>
+
 <body>
 <?php
+// DatenBank verbindung
 $pdo = new PDO("mysql:host=localhost;dbname=db_training", "root", "");
+
 $filename = "E:/Lena/Git/Project1/log_files/test.log";
+function save_log_file($pdo, $filename)
+{
+        if ($handle = fopen($filename, "r")) {
+            $pdo->exec("TRUNCATE TABLE log_entries");
+            while (($line = fgets($handle)) !== false) {
 
-if ($handle = fopen($filename, "r")) {
-    while (($line = fgets($handle)) !== false) {
+                // pro Zeile alle wichtige Werte holen:
+                $log = htmlspecialchars($line);
 
-        // pro Zeile alle wichtige Werte holen:
-        $log = htmlspecialchars($line);
+                //IP Adresse
+                $ip_adress = strtok($log, " ") ?: null;
 
-        //IP Adresse
-        $ip_adress = strtok($log, " ") ?: null;
+                //Datum/Zeit des Zugriffs
+                if (preg_match('/\[(.*?)\]/', $log, $matches)) {
+                    $access_time = new DateTime($matches[1]);
+                    $access_time = $access_time->format("Y-m-d H:i:s");
+                } else {
+                    $access_time = null;
+                }
 
-        //Datum/Zeit des Zugriffs
-        if (preg_match('/\[(.*?)\]/', $log, $matches)) {
-            $access_time = new DateTime($matches[1]);
-            $access_time = $access_time->format("Y-m-d H:i:s");
-        } else {
-            $access_time = null;
-        }
-
-        //specs
-        $specs_position = strpos($log, "specs=");
-        // Wenn das Wort gefunden wurde
-        if ($specs_position !== false) {
-            // Den String nach dem Wort abschneiden
-            $specs = strtok(substr($log, $specs_position + strlen("specs=")), " ");
-        }
-        // serial
-        $serial_positon = strpos($log, "serial=");
-        if ($specs_position !== false) {
-            // Den String nach dem Wort abschneiden
-            $serial = strtok(substr($log, $serial_positon + strlen("serial=")), " ");
-        } else {
-            $serial = null;
-        }
-        $mac = null;
-        $cpu = null;
-        if (isset($specs)) {
-            $decoded = base64_decode($specs);
-            if ($decoded !== false) {
-                $json = @gzdecode($decoded);
-                if ($json !== false) {
-                    $specObj = json_decode($json, true);
-                    if (is_array($specObj)) {
-                        $mac = $specObj['mac'] ?? null;
-                        $cpu = $specObj['cpu'] ?? null;
+                //specs
+                $specs_position = strpos($log, "specs=");
+                // Wenn das Wort gefunden wurde
+                if ($specs_position !== false) {
+                    // Den String nach dem Wort abschneiden
+                    $specs = strtok(substr($log, $specs_position + strlen("specs=")), " ");
+                }
+                // serial
+                $serial_positon = strpos($log, "serial=");
+                if ($specs_position !== false) {
+                    // Den String nach dem Wort abschneiden
+                    $serial = strtok(substr($log, $serial_positon + strlen("serial=")), " ");
+                } else {
+                    $serial = null;
+                }
+                $mac = null;
+                $cpu = null;
+                if (isset($specs)) {
+                    $decoded = base64_decode($specs);
+                    if ($decoded !== false) {
+                        $json = @gzdecode($decoded);
+                        if ($json !== false) {
+                            $specObj = json_decode($json, true);
+                            if (is_array($specObj)) {
+                                $mac = $specObj['mac'] ?? null;
+                                $cpu = $specObj['cpu'] ?? null;
+                            }
+                        }
                     }
                 }
+                // Datenbank Insert
+                $sql_insert = "INSERT INTO log_entries (ip_adress, serial, mac, cpu, access_time) VALUES (:ip_adress, :serial,:mac, :cpu, :access_time)";
+                $stmt = $pdo->prepare($sql_insert);
+                $stmt->execute([
+                    ':ip_adress' => $ip_adress,
+                    ':serial' => $serial,
+                    ':mac' => $mac,
+                    ':cpu' => $cpu,
+                    ':access_time' => $access_time,
+                ]);
             }
-        }
-        // Datenbank Verbindung:
+            fclose($handle);
+            echo "<div> - Die Datei <strong>" . basename($filename) . "</strong> wurde erfolgreich gespeichert &#10004;</div>";
 
-        $sql_insert = "INSERT INTO log_entries (ip_adress, serial, mac, cpu, access_time) VALUES (:ip_adress, :serial,:mac, :cpu, :access_time)";
-        $stmt = $pdo->prepare($sql_insert);
-        $stmt->execute([
-            ':ip_adress' => $ip_adress,
-            ':serial' => $serial,
-            ':mac' => $mac,
-            ':cpu' => $cpu,
-            ':access_time' => $access_time,
-        ]);
+    } else {
+        echo "<div> - Die Datei <strong>" . basename($filename) . "</strong> existiert nicht!</div>";
     }
-    fclose($handle);
-} else {
-    echo "File $filename existiert nicht!";
 }
-// get 10 Lizenz-Seriennummern, die am häufigsten versuchen, auf den Server zuzugreifen
-$license = $pdo->query("
-    SELECT serial, access_time, COUNT(*) AS access_count
-    FROM log_entries
-    GROUP BY serial
-    ORDER BY access_count DESC
-    LIMIT 10
-");
-?>
-<!--Tabelle mit Ergebnissen-->
-<table style="border: 1px solid #000000;">
-    <tr>
-        <th style="border: 1px solid #000000; padding: 5px 10px;">Lizens-Seriennummer</th>
-        <th style="border: 1px solid #000000; padding: 5px 10px;">Anzhal des Zugriffs</th>
-        <th style="border: 1px solid #000000; padding: 5px 10px;">Datum des Zugriffs</th>
-    </tr>
 
-    <?php foreach ($license as $row):
+// get 10 Lizenz-Seriennummern, die am häufigsten versuchen, auf den Server zuzugreifen
+function get_frequent_license($pdo)
+{
+    $license = $pdo->query("
+        SELECT serial, access_time, COUNT(*) AS access_count
+        FROM log_entries
+        GROUP BY serial
+        ORDER BY access_count DESC
+        LIMIT 10
+    ");
+
+//    Tabelle mit Ergebnissen
+    echo "<h3>Top 10 der meistgenutzten Lizenz-Seriennummern am </h3>";
+    echo '<table style="border: 1px solid #000000;">
+        <tr>
+            <th style="border: 1px solid #000000; padding: 5px 10px;">Platz</th>
+            <th style="border: 1px solid #000000; padding: 5px 10px;">Lizens-Seriennummer</th>
+            <th style="border: 1px solid #000000; padding: 5px 10px;">Anzhal des Zugriffs</th>
+            <th style="border: 1px solid #000000; padding: 5px 10px;">Datum des Zugriffs</th>
+        </tr>' . get_rows($license);
+}
+function get_rows($license)
+{
+    $rows = '';
+    $i = 0;
+    foreach ($license as $row) {
+        $i++;
         $datum = new DateTime($row['access_time']);
         $datum = $datum->format("Y-m-d");
-        ?>
-        <tr>
-            <td style="border-bottom: 1px solid #000000;  border-left: 1px solid #000000; border-right: 1px solid #000000; padding: 5px 10px;"> <?php echo $row['serial']; ?></td>
-            <td style="border-bottom: 1px solid #000000;  border-right: 1px solid #000000; padding: 5px 10px; text-align: center;"> <?php echo $row['access_count']; ?></td>
-            <td style="border-bottom: 1px solid #000000;border-right: 1px solid #000000; text-align: center;"> <?php echo $datum; ?></td>
-        </tr>
-    <?php endforeach; ?>
+        $rows .= '<tr>
+                <td style="border-bottom: 1px solid #000000;  border-left: 1px solid #000000; border-right: 1px solid #000000; padding: 5px 10px;">' . $i . '</td>
+                <td style="border-bottom: 1px solid #000000;  border-left: 1px solid #000000; border-right: 1px solid #000000; padding: 5px 10px;">' . $row['serial'] . '</td>
+                <td style="border-bottom: 1px solid #000000;  border-right: 1px solid #000000; padding: 5px 10px; text-align: center;">' . $row['access_count'] . '</td>
+                <td style="border-bottom: 1px solid #000000;border-right: 1px solid #000000; text-align: center;">' . $datum . '</td>
+            </tr>';
+    }
+    return $rows;
+}
+
+?>
+<h1>Analysiere die Logdatei</h1>
+<div class="button-wrapper">
+    <div class="import">
+        <form method="get">
+            <button class="buttons" type="submit" name="log_import">Daten importieren</button>
+        </form>
+    </div>
+    <div class="tables">
+        <form method="post">
+            <button class="buttons" type="submit" name="evaluation">Auswerten</button>
+        </form>
+    </div>
+</div>
+<div class="status">
+    <?php
+    if (isset($_GET['log_import'])) {
+        save_log_file($pdo, $filename);
+    } ?>
+</div>
+<div class="results">
+    <?php
+    if (isset($_POST['evaluation'])) {
+        get_frequent_license($pdo);
+    }
+    ?>
+</div>
 </body>
-</html> //
+</html>
